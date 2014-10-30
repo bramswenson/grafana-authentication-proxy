1a9462c8d92fa8f0027cff7425b4e2cf684d8a82
Bram Swenson <bram@craniumisajar.com>
2014-10-30 11:17:02 -0400

    Enable google oauth

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var path = require('path');
 var http = require('http');
 var express = require('express');
 
-var configurator = require('./lib/configurator')
+var configurator = require('./lib/configurator');
+var google_oauth = require('./lib/google-oauth');
 var config = require('./config');
 
 var app = express();
@@ -16,7 +17,7 @@ var app = express();
 app.use(express.logger());
 app.use(express.cookieParser());
 app.use(express.session({ secret: config.cookie_secret }));
-//require('./lib/google-oauth').configureOAuth(express, app, config);
+google_oauth.configureOAuth(express, app, config);
 app.use(express.compress());
 app.use('/config.js', configurator);
 app.use(express.static(process.env.APP_ROOT + '/grafana-1.8.1', {maxAge: config.brower_cache_maxage || 0}));
