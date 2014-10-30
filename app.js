250b0964fde743fd0d471b19d9145ab1ed3f2868
Bram Swenson <bram@craniumisajar.com>
2014-10-30 10:33:15 -0400

    maybe the symlink?

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.cookieParser());
 app.use(express.session({ secret: config.cookie_secret }));
 //require('./lib/google-oauth').configureOAuth(express, app, config);
 app.use(express.compress());
-app.use(express.static(path.resolve(process.env.APP_ROOT + '/grafana'), {maxAge: config.brower_cache_maxage || 0}));
+app.use(express.static(process.env.APP_ROOT + '/grafana-1.8.1', {maxAge: config.brower_cache_maxage || 0}));
 
 http.createServer(app).listen(config.listen_port);
 console.log('Server listening on ' + config.listen_port);
