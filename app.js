640ef754ca740dd97debce22c34a272123acbf6f
Bram Swenson <bram@craniumisajar.com>
2014-10-30 11:11:51 -0400

    Get the urls from the env and serve the config.js via express

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ if (process.env.NODE_ENV != 'production') {
 }
 process.env.APP_ROOT = process.cwd()
 
-var express = require('express');
-var http = require('http');
 var path = require('path');
+var http = require('http');
+var express = require('express');
 
+var configurator = require('./lib/configurator')
 var config = require('./config');
+
 var app = express();
 
 app.use(express.logger());
@@ -16,6 +18,7 @@ app.use(express.cookieParser());
 app.use(express.session({ secret: config.cookie_secret }));
 //require('./lib/google-oauth').configureOAuth(express, app, config);
 app.use(express.compress());
+app.use('/config.js', configurator);
 app.use(express.static(process.env.APP_ROOT + '/grafana-1.8.1', {maxAge: config.brower_cache_maxage || 0}));
 
 http.createServer(app).listen(config.listen_port);
---
+++ grafana_config.js
@@ -0,0 +1,62 @@
+// == Configuration
+// config.js is where you will find the core Grafana configuration. This file contains parameter that
+// must be set before Grafana is run for the first time.
+
+define(['settings'],
+function (Settings) {
+  return new Settings({
+    datasources: {
+      // these are replaced with express middleware
+      influxdb: {
+        type: 'influxdb',
+        url: 'INFLUXDB_URL',
+        username: 'INFLUXDB_USER',
+        password: 'INFLUXDB_PASS',
+      },
+      grafana: {
+        type: 'influxdb',
+        url: 'GRAFANA_INFLUXDB_URL',
+        username: 'GRAFANA_INFLUXDB_USER',
+        password: 'GRAFANA_INFLUXDB_PASS',
+        grafanaDB: true
+      },
+    },
+
+    /* Global configuration options
+    * ========================================================
+    */
+
+    // specify the limit for dashboard search results
+    search: {
+      max_results: 20
+    },
+
+    // default home dashboard
+    default_route: '/dashboard/file/default.json',
+
+    // set to false to disable unsaved changes warning
+    unsaved_changes_warning: true,
+
+    // set the default timespan for the playlist feature
+    // Example: "1m", "1h"
+    playlist_timespan: "1m",
+
+    // If you want to specify password before saving, please specify it bellow
+    // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
+    admin: {
+      password: ''
+    },
+
+    // Change window title prefix from 'Grafana - <dashboard title>'
+    window_title_prefix: 'Grafana - ',
+
+    // Add your own custom panels
+    plugins: {
+      // list of plugin panels
+      panels: [],
+      // requirejs modules in plugins folder that should be loaded
+      // for example custom datasources
+      dependencies: [],
+    }
+  });
+});
