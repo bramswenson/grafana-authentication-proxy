5bb8362a968f041901f9845aa2238f8bf2286d38
Bram Swenson <bram@craniumisajar.com>
2014-10-30 10:14:43 -0400

    trim down, clean up

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-/**
- * Hosts the latest grafana and elasticsearch behind Google OAuth2 Authentication
- * with nodejs and express.
- * License: MIT
- * Copyright: Funplus Game Inc.
- * Author: strima
- * Original Author: Fang Li.
- * Project: https://github.com/strima/grafana-authentication-proxy
- */
 var dotenv = require('dotenv');
 if (process.env.NODE_ENV != 'production') {
   dotenv.load();
 }
 
 var express = require('express');
-var https = require('https');
 var http = require('http');
-var fs = require('fs');
+var path = require('path');
 
 var config = require('./config');
 var app = express();
 
 app.use(express.logger());
-
-console.log('Server starting...');
-
-if (!config.base_path) {  config.base_path="" }
-
 app.use(express.cookieParser());
 app.use(express.session({ secret: config.cookie_secret }));
 //require('./lib/google-oauth').configureOAuth(express, app, config);
-
-// Serve all grafana frontend files
 app.use(express.compress());
-app.use(config.base_path + '/', express.static(__dirname + '/grafana', {maxAge: config.brower_cache_maxage || 0}));
+app.use(express.static(path.resolve(__dirname + '/grafana'), {maxAge: config.brower_cache_maxage || 0}));
 
-
-run();
-
-function run() {
-  if (config.enable_ssl_port === true) {
-    var options = {
-      key: fs.readFileSync(config.ssl_key_file),
-      cert: fs.readFileSync(config.ssl_cert_file),
-    };
-    https.createServer(options, app).listen(config.listen_port_ssl);
-    console.log('Server listening on ' + config.listen_port_ssl + '(SSL)');
-  }
-  http.createServer(app).listen(config.listen_port);
-  console.log('Server listening on ' + config.listen_port);
-}
+http.createServer(app).listen(config.listen_port);
+console.log('Server listening on ' + config.listen_port);
--- config.js
+++ config.js
@@ -1,24 +1,9 @@
 module.exports =  {
-    "base_path": "",
-
-    ////////////////////////////////////
-    // Proxy server configurations
-    ////////////////////////////////////
-    // Which port listen to
     "listen_port": process.env.PORT,
     // Control HTTP max-Age header. Whether the browser cache static grafana files or not?
     // 0 for no-cache, unit in millisecond, default to 0
     // We strongly recommand you set to a larger number such as 2592000000(a month) to get a better loading speed
     "brower_cache_maxage": 0,
-    // Enable SSL protocol
-    "enable_ssl_port": false,
-        // The following settings are valid only when enable_ssl_port is true
-        "listen_port_ssl": 4444,
-        // Use absolute path for the key file
-        "ssl_key_file": "POINT_TO_YOUR_SSL_KEY",
-        // Use absolute path for the certification file
-        "ssl_cert_file": "POINT_TO_YOUR_SSL_CERT",
-
     "cookie_secret": process.env.COOKIE_SECRET,
 
     // We use the following redirect URI:
